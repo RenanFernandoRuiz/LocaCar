b9a2eb6cabffa23c1e6eeacf7416d953c376295f
RenanFernandoRuiz <renanfernandor908@gmail.com>
2024-10-25 09:17:10 -0300

    Footer concluído

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer className="footer text-center p-4">
+      <div className="container">
+        <h5>Contatos</h5>
+        <p>Email: contato@locacar.com</p>
+        <p>Telefone: (00) 1234-5678</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.jsx'
 
